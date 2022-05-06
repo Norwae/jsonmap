5afb22672a4a43666364c64d5f6e06d94339e5c5
Elisabeth 'TerraNova' Schulz <schulz.elisabeth.terra@gmail.com>
2022-05-06 13:03:19 +0200

    WIP for the parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,77 +1,116 @@
+use std::cell::RefCell;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+
 use nom::branch::alt;
 use nom::bytes::complete::{escaped, tag};
 use nom::character::complete::{alpha1, digit1, none_of, space0};
-use nom::combinator::{map_res, opt, recognize};
-use nom::error::{Error as NomError, FromExternalError};
-use nom::error::ErrorKind;
+use nom::combinator::{map, map_res, opt, recognize};
+use nom::Err;
+use nom::error::Error as NomError;
 use nom::IResult;
 use nom::multi::{many0, many1, separated_list1};
+use nom::number::complete::{double, f64, float};
 use nom::sequence::{delimited, terminated, tuple};
 
 use crate::query as q;
 use crate::query::Query;
 
+#[derive(Debug)]
+pub struct ParseError {
+    message: String
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str("ParseError(")?;
+        f.write_str(&self.message)?;
+        f.write_str(")")
+    }
+}
+
+impl Error for ParseError {}
+
+impl <A: Debug> From<Err<A>> for ParseError {
+    fn from(e: Err<A>) -> Self {
+        ParseError {
+            message: format!("Nom Error: {:?}", e)
+        }
+    }
+}
+
 pub trait QueryBuilder {
-    fn construct_array(declarations: Vec<Box<dyn Query>>) -> Box<dyn Query>;
-    fn construct_object(declarations: Vec<(&str, Box<dyn Query>)>) -> Box<dyn Query>;
-    fn select_member(name: & str) -> Box<dyn Query>;
-    fn select_index(idx: usize) -> Box<dyn Query>;
-    fn and_then(a: Box<dyn Query>, b: Box<dyn Query>) -> Box<dyn Query>;
-    fn constant_bool(v: bool) -> Box<dyn Query>;
-    fn constant_nr(nr: f64) -> Box<dyn Query>;
-    fn constant_string(string: &str) -> Box<dyn Query>;
-    fn null() -> Box<dyn Query>;
-    fn root() -> Box<dyn Query>;
-    fn array_size() -> Box<dyn Query>;
+    fn construct_array(&mut self, declarations: Vec<Box<dyn Query>>) -> Box<dyn Query>;
+    fn construct_object(&mut self, declarations: Vec<(&str, Box<dyn Query>)>) -> Box<dyn Query>;
+    fn select_member(&mut self, name: &str) -> Box<dyn Query>;
+    fn select_index(&mut self, idx: usize) -> Box<dyn Query>;
+    fn and_then(&mut self, a: Box<dyn Query>, b: Box<dyn Query>) -> Box<dyn Query>;
+    fn constant_bool(&mut self, v: bool) -> Box<dyn Query>;
+    fn constant_nr(&mut self, nr: f64) -> Box<dyn Query>;
+    fn constant_string(&mut self, string: &str) -> Box<dyn Query>;
+    fn null(&mut self) -> Box<dyn Query>;
+    fn root(&mut self) -> Box<dyn Query>;
+    fn array_size(&mut self) -> Box<dyn Query>;
 }
 
 #[derive(Debug, Default)]
 struct DefaultQueryBuilder;
 
 impl QueryBuilder for DefaultQueryBuilder {
-    fn construct_array(declarations: Vec<Box<dyn Query>>) -> Box<dyn Query> {
+    fn construct_array(&mut self, declarations: Vec<Box<dyn Query>>) -> Box<dyn Query> {
         Box::new(q::construct_array(declarations))
     }
 
-    fn construct_object(declarations: Vec<(&str, Box<dyn Query>)>) -> Box<dyn Query> {
+    fn construct_object(&mut self, declarations: Vec<(&str, Box<dyn Query>)>) -> Box<dyn Query> {
         Box::new(q::construct_object(declarations))
     }
 
-    fn select_member(name: &str) -> Box<dyn Query> {
+    fn select_member(&mut self, name: &str) -> Box<dyn Query> {
         Box::new(q::select_member(name))
     }
 
-    fn select_index(idx: usize) -> Box<dyn Query> {
+    fn select_index(&mut self, idx: usize) -> Box<dyn Query> {
         Box::new(q::select_index(idx))
     }
 
-    fn and_then(a: Box<dyn Query>, b: Box<dyn Query>) -> Box<dyn Query> {
+    fn and_then(&mut self, a: Box<dyn Query>, b: Box<dyn Query>) -> Box<dyn Query> {
         Box::new(q::and_then(a, b))
     }
 
-    fn constant_bool(v: bool) -> Box<dyn Query> {
+    fn constant_bool(&mut self, v: bool) -> Box<dyn Query> {
         Box::new(q::constant_bool(v))
     }
 
-    fn constant_nr(nr: f64) -> Box<dyn Query> {
+    fn constant_nr(&mut self, nr: f64) -> Box<dyn Query> {
         Box::new(q::constant_nr(nr))
     }
 
-    fn constant_string(string: &str) -> Box<dyn Query> {
+    fn constant_string(&mut self, string: &str) -> Box<dyn Query> {
         Box::new(q::constant_string(string))
     }
 
-    fn null() -> Box<dyn Query> {
+    fn null(&mut self) -> Box<dyn Query> {
         Box::new(q::null())
     }
 
-    fn root() -> Box<dyn Query> {
+    fn root(&mut self) -> Box<dyn Query> {
         Box::new(q::root())
     }
 
-    fn array_size() -> Box<dyn Query> {
+    fn array_size(&mut self) -> Box<dyn Query> {
         Box::new(q::array_size())
     }
+}
+
+fn literal<'s, QB: QueryBuilder>(qb: &RefCell<QB>, i: &'s str) -> IResult<&'s str, Box<dyn Query>> {
+    alt((
+        map(double, |f: f64| qb.borrow_mut().constant_nr(f)),
+        map(delimited(tag("'"), recognize(many1(none_of("'"))), tag("'")), |str| qb.borrow_mut().constant_string(str)),
+        map(tag("true"), |_| qb.borrow_mut().constant_bool(true)),
+        map(tag("false"), |_| qb.borrow_mut().constant_bool(false)),
+        map(tag("null"), |_| qb.borrow_mut().null()),
+        map(tag("@"), |_| qb.borrow_mut().root()),
+    ))(i)
 }
 
 fn simple_name(i: &str) -> IResult<&str, &str> {
@@ -87,17 +126,17 @@ fn name(i: &str) -> IResult<&str, &str> {
 }
 
 fn index(i: &str) -> IResult<&str, usize> {
-    terminated(delimited(tag("["),map_res(
+    terminated(delimited(tag("["), map_res(
         digit1,
-        |slice: &str| slice.parse::<usize>()
+        |slice: &str| slice.parse::<usize>(),
     ), tag("]")), space0)(i)
 }
 
-fn path_segment<QB: QueryBuilder>(i: &str) -> IResult<&str, Box<dyn Query>> {
+fn path_segment<'s, QB: QueryBuilder>(qb: &RefCell<QB>, i: &'s str) -> IResult<&'s str, Box<dyn Query>> {
     let (rest, (field, indices)) = tuple((name, many0(index)))(i)?;
-    let select_name = QB::select_member(field);
+    let select_name = qb.borrow_mut().select_member(field);
     let q = indices.into_iter().fold(select_name, |query, next| {
-        QB::and_then(query, QB::select_index(next))
+        qb.borrow_mut().and_then(query, qb.borrow_mut().select_index(next))
     });
 
     Ok((rest, q))
@@ -107,135 +146,139 @@ fn path_segment_sep(i: &str) -> IResult<&str, &str> {
     terminated(tag("."), space0)(i)
 }
 
-fn path<QB: QueryBuilder>(i: &str) -> IResult<&str, Box<dyn Query>> {
-    let (rest, mut qs) = separated_list1(path_segment_sep, path_segment::<QB>)(i)?;
+fn path<'s, QB: QueryBuilder>(qb: &RefCell<QB>, i: &'s str) -> IResult<&'s str, Box<dyn Query>> {
+    let (rest, mut qs) = separated_list1(path_segment_sep, |input| path_segment(qb, input))(i)?;
     let mut result = qs.pop().unwrap();
 
     while let Some(q) = qs.pop() {
-        result = QB::and_then(q, result)
+        result = qb.borrow_mut().and_then(q, result)
     }
 
     Ok((rest, result))
 }
 
-pub fn parse_query(input: &str) -> Box<dyn Query> {
-    parse_query_with::<DefaultQueryBuilder>(input)
+pub fn parse_query(input: &str) -> Result<Box<dyn Query>, ParseError> {
+    parse_query_with(DefaultQueryBuilder, input)
 }
 
 
-pub fn parse_query_with<T: QueryBuilder>(input: &str) -> Box<dyn Query> {
-    todo!()
+pub fn parse_query_with<T: QueryBuilder>(qb: T, input: &str) -> Result<Box<dyn Query>, ParseError>  {
+    let qb = RefCell::new(qb);
+    let (rest, parsed) = alt((
+        |s|literal(&qb, s),
+        |s|path(&qb, s)
+    ))(input)?;
+
+    if rest.trim() == "" {
+        Ok(parsed)
+    } else {
+        Err(ParseError{
+            message: format!("Trailing input could not be parsed, found {}", rest)
+        })
+    }
 }
 
 #[cfg(test)]
 mod test {
     use std::borrow::Cow;
     use std::cell::RefCell;
+    use std::fmt::{Display, format};
     use std::mem::swap;
+
     use crate::model::JsonElement;
-    use crate::parser::{path, QueryBuilder};
+    use crate::parser::{literal, path, QueryBuilder};
     use crate::query::{Query, QueryContext};
 
-
-    #[derive(Clone,Debug,Eq, PartialEq)]
+    #[derive(Clone, Debug, Eq, PartialEq)]
     struct QueryBuilderAction {
         method: &'static str,
-        args: String
-    }
-
-    static mut ACTIONS: RefCell<Vec<QueryBuilderAction>> = RefCell::new(Vec::new());
-
-    fn reset_actions() {
-        unsafe {
-            ACTIONS.borrow_mut().clear()
-        }
-    }
-
-    fn snapshot_actions() -> Vec<QueryBuilderAction>{
-        unsafe {
-            let mut result = Vec::new();
-            swap(&mut result, ACTIONS.borrow_mut().as_mut());
-            result
-        }
-    }
-
-    fn push_action<T : ToString>(method: &'static str, args: T) {
-        let args = args.to_string();
-        unsafe {
-            ACTIONS.borrow_mut().push(QueryBuilderAction { method, args })
-        }
+        args: String,
     }
 
 
-    struct TestUtil;
-    impl Query for TestUtil {
+    struct TestUtil(Vec<QueryBuilderAction>);
+
+    struct NilQuery;
+
+    impl Query for NilQuery {
         fn perform<'q>(&self, ctx: QueryContext<'q>) -> Cow<'q, JsonElement> {
             ctx.current_element
         }
     }
 
+    impl TestUtil {
+        fn new() -> RefCell<Self> {
+            RefCell::new(TestUtil(Vec::new()))
+        }
+
+        fn push_action<T: Display>(&mut self, method: &'static str, args: T) {
+            self.0.push(QueryBuilderAction { method, args: format!("{}", args) })
+        }
+    }
+
     impl QueryBuilder for TestUtil {
-        fn construct_array(declarations: Vec<Box<dyn Query>>) -> Box<dyn Query> {
-            push_action("construct_array", "");
-            Box::new(TestUtil)
+        fn construct_array(&mut self, declarations: Vec<Box<dyn Query>>) -> Box<dyn Query> {
+            self.push_action("construct_array", "");
+            Box::new(NilQuery)
         }
 
-        fn construct_object(declarations: Vec<(&str, Box<dyn Query>)>) -> Box<dyn Query> {
-            push_action("construct_object", "");
-            Box::new(TestUtil)
+        fn construct_object(&mut self, declarations: Vec<(&str, Box<dyn Query>)>) -> Box<dyn Query> {
+            self.push_action("construct_object", "");
+            Box::new(NilQuery)
         }
 
-        fn select_member(name: &str) -> Box<dyn Query> {
-            push_action("select_member", name);
-            Box::new(TestUtil)
+        fn select_member(&mut self, name: &str) -> Box<dyn Query> {
+            self.push_action("select_member", name);
+            Box::new(NilQuery)
         }
 
-        fn select_index(idx: usize) -> Box<dyn Query> {
-            push_action("select_index", format!("{}", idx));
-            Box::new(TestUtil)
+        fn select_index(&mut self, idx: usize) -> Box<dyn Query> {
+            self.push_action("select_index", format!("{}", idx));
+            Box::new(NilQuery)
         }
 
-        fn and_then(a: Box<dyn Query>, b: Box<dyn Query>) -> Box<dyn Query> {
-            push_action("and_then", "");
-            Box::new(TestUtil)
+        fn and_then(&mut self, a: Box<dyn Query>, b: Box<dyn Query>) -> Box<dyn Query> {
+            self.push_action("and_then", "");
+            Box::new(NilQuery)
         }
 
-        fn constant_bool(v: bool) -> Box<dyn Query> {
-            push_action("constant_bool", v);
-            Box::new(TestUtil)
+        fn constant_bool(&mut self, v: bool) -> Box<dyn Query> {
+            self.push_action("constant_bool", v);
+            Box::new(NilQuery)
         }
 
-        fn constant_nr(nr: f64) -> Box<dyn Query> {
-            push_action("constant_nr", nr);
-            Box::new(TestUtil)
+        fn constant_nr(&mut self, nr: f64) -> Box<dyn Query> {
+            self.push_action("constant_nr", nr);
+            Box::new(NilQuery)
         }
 
-        fn constant_string(string: &str) -> Box<dyn Query> {
-            push_action("constant_string", string);
-            Box::new(TestUtil)
+        fn constant_string(&mut self, string: &str) -> Box<dyn Query> {
+            self.push_action("constant_string", string);
+            Box::new(NilQuery)
         }
 
-        fn null() -> Box<dyn Query> {
-            push_action("null", "");
-            Box::new(TestUtil)
+        fn null(&mut self) -> Box<dyn Query> {
+            self.push_action("null", "");
+            Box::new(NilQuery)
         }
 
-        fn root() -> Box<dyn Query> {
-            push_action("root", "");
-            Box::new(TestUtil)
+        fn root(&mut self) -> Box<dyn Query> {
+            self.push_action("root", "");
+            Box::new(NilQuery)
         }
 
-        fn array_size() -> Box<dyn Query> {
-            push_action("array_size", "");
-            Box::new(TestUtil)
+        fn array_size(&mut self) -> Box<dyn Query> {
+            self.push_action("array_size", "");
+            Box::new(NilQuery)
         }
     }
 
 
     #[test]
     fn parse_simple_path() {
-        assert!(path::<TestUtil>("a.b.c").is_ok());
-        assert_eq!(snapshot_actions(), vec![
+        let mut test = TestUtil::new();
+        assert!(path(&test, "a.b.c").is_ok());
+        assert_eq!(test.into_inner().0, vec![
             QueryBuilderAction { method: "select_member", args: "a".to_string() },
             QueryBuilderAction { method: "select_member", args: "b".to_string() },
             QueryBuilderAction { method: "select_member", args: "c".to_string() },
@@ -245,9 +288,10 @@ mod test {
     }
 
     #[test]
-    fn parse_with_lots_of_whitespace(){
-        assert!(path::<TestUtil>("d  .   e .  f  ").is_ok());
-        assert_eq!(snapshot_actions(), vec![
+    fn parse_with_lots_of_whitespace() {
+        let mut test = TestUtil::new();
+        assert!(path(&mut test, "d  .   e .  f  ").is_ok());
+        assert_eq!(test.into_inner().0, vec![
             QueryBuilderAction { method: "select_member", args: "d".to_string() },
             QueryBuilderAction { method: "select_member", args: "e".to_string() },
             QueryBuilderAction { method: "select_member", args: "f".to_string() },
@@ -259,13 +303,36 @@ mod test {
 
     #[test]
     fn parse_quoted_field() {
-        assert!(path::<TestUtil>("a.\"complex quoted field name, including special characters like . or []\".c").is_ok());
-        assert_eq!(snapshot_actions(), vec![
+        let mut test = TestUtil::new();
+        assert!(path(&mut test, "a.\"complex quoted field name, including special characters like . or []\".c").is_ok());
+        assert_eq!(test.into_inner().0, vec![
             QueryBuilderAction { method: "select_member", args: "a".to_string() },
             QueryBuilderAction { method: "select_member", args: "complex quoted field name, including special characters like . or []".to_string() },
             QueryBuilderAction { method: "select_member", args: "c".to_string() },
             QueryBuilderAction { method: "and_then", args: "".to_string() },
             QueryBuilderAction { method: "and_then", args: "".to_string() },
         ]);
+    }
+
+    #[test]
+    fn parse_literals() {
+        let mut test = TestUtil::new();
+        assert!(literal(&test, "true").is_ok());
+        assert!(literal(&test, "false").is_ok());
+        assert!(literal(&test, "16").is_ok());
+        assert!(literal(&test, "-17.3e-12").is_ok());
+        assert!(literal(&test, "'Cheese Borger'").is_ok());
+        assert!(literal(&test, "null").is_ok());
+        assert!(literal(&test, "@").is_ok());
+
+        assert_eq!(test.into_inner().0, vec![
+            QueryBuilderAction { method: "constant_bool", args: "true".to_string() },
+            QueryBuilderAction { method: "constant_bool", args: "false".to_string() },
+            QueryBuilderAction { method: "constant_nr", args: "16".to_string() },
+            QueryBuilderAction { method: "constant_nr", args: "-0.0000000000173".to_string() },
+            QueryBuilderAction { method: "constant_string", args: "Cheese Borger".to_string() },
+            QueryBuilderAction { method: "null", args: "".to_string() },
+            QueryBuilderAction { method: "root", args: "".to_string() },
+        ])
     }
 }
